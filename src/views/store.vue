0665533f0035d160d54bbd787a8f0ced2907779a
ccsunnyfd <chzhyfd@126.com>
2019-08-02 10:21:41 +0800

    test sibling components communication(through parent component)

--- src/views/store.vue
+++ src/views/store.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <!-- <a-input :value="inputValue" @input="handleInput" /> -->
-    <a-input v-model="inputValue" />
-    <p>{{  inputValue  }}</p>
+    <!-- <a-input v-model="inputValue" /> -->
+    <a-input @input="handleInput" />
+    <!-- <p>{{  inputValue  }}</p> -->
+    <a-show :content="inputValue" />
   </div>
 </template>
 <script>
 import AInput from "_c/AInput.vue";
+import AShow from "_c/AShow.vue";
 
 export default {
   name: "store",
@@ -16,12 +19,16 @@ export default {
     };
   },
   components: {
-    AInput
+    AInput,
+    AShow
   },
   methods: {
     // handleInput(val) {
     //   this.inputValue = val;
     // }
+    handleInput(val) {
+      this.inputValue = val;
+    }
   }
 };
 </script>
