f7d7bd4e3f798fe95be3b10e35c98c351450651a
ccsunnyfd <chzhyfd@126.com>
2019-08-02 10:02:06 +0800

    test Parent and Child component communication

---
+++ src/components/AInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <input @input="handleInput" :value="value" />
+</template>
+
+<script>
+export default {
+  name: "AInput",
+  props: {
+    value: {
+      type: [String, Number],
+      default: ""
+    }
+  },
+  methods: {
+    handleInput() {
+      const value = event.target.value;
+      this.$emit("input", value); // 强调单向数据流，子组件不应该修改父组件的传值，通过事件传递
+    }
+  }
+};
+</script>
---
+++ src/views/store.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- <a-input :value="inputValue" @input="handleInput" /> -->
+    <a-input v-model="inputValue" />
+    <p>{{  inputValue  }}</p>
+  </div>
+</template>
+<script>
+import AInput from "_c/AInput.vue";
+
+export default {
+  name: "store",
+  data() {
+    return {
+      inputValue: ""
+    };
+  },
+  components: {
+    AInput
+  },
+  methods: {
+    // handleInput(val) {
+    //   this.inputValue = val;
+    // }
+  }
+};
+</script>
