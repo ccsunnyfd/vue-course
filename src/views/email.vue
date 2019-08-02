80cce55c8795061f9e7167e7293c4887def69af7
ccsunnyfd <chzhyfd@126.com>
2019-08-02 10:45:13 +0800

    test communication between two view components(through bus)

--- src/views/email.vue
+++ src/views/email.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
-    email: lison16new
+  <div class="email">
+    <button @click="handleClick">按我</button>
   </div>
 </template>
+<script>
+export default {
+  mounted() {
+    console.log(this.$bus);
+  },
+  methods: {
+    handleClick() {
+      this.$bus.$emit("on-click", "hello");
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.email {
+  border: 1px solid green;
+}
+</style>
+
+
--- src/views/tel.vue
+++ src/views/tel.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
-    tel: 666666
+  <div class="tel">
+    {{ message }}
   </div>
 </template>
+<script>
+export default {
+  name: "",
+  data() {
+    return {
+      message: ""
+    };
+  },
+  mounted() {
+    this.$bus.$on("on-click", mes => {
+      this.message = mes;
+    });
+  }
+};
+</script>
+
+<style lang="less">
+.tel {
+  border: 1px solid red;
+}
+</style>
+
