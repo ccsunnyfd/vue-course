80247e0f073bccb8e88ce8aad28b63e099ef128d
ccsunnyfd <chzhyfd@126.com>
2019-08-03 20:22:56 +0800

    test Vuex actions(mock async fetch data_ES6 elegant way style)

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,15 +1,23 @@
 import { getAppName } from '@/api/app.js'
 
 const actions = {
-  updateAppName({ commit }) {
-    getAppName().then(res => {
-      // console.log(res)
-      // const { code, info: { appName } } = res
-      const { info: { appName } } = res
+  // updateAppName({ commit }) {
+  //   getAppName().then(res => {
+  //     // console.log(res)
+  //     // const { code, info: { appName } } = res
+  //     const { info: { appName } } = res
+  //     commit('SET_APP_NAME', appName)
+  //   }).catch(err => {
+  //     console.log(err)
+  //   })
+  // }
+  async updateAppName({ commit }) {
+    try {
+      const { info: { appName } } = await getAppName()
       commit('SET_APP_NAME', appName)
-    }).catch(err => {
+    } catch (err) {
       console.log(err)
-    })
+    }
   }
 }
 export default actions
