cec6e6cce2e8b17e3d70d5fdac5ea898d090babf
ccsunnyfd <chzhyfd@126.com>
2019-08-03 12:02:56 +0800

    test Vuex(get state using ES6 createNamespacedHelpers mapState under namespace circumstance)

--- src/store/module/user.js
+++ src/store/module/user.js
@@ -9,6 +9,7 @@ const actions = {
 }
 
 export default {
+  namespaced: true,
   state,
   mutations,
   actions
--- src/views/store.vue
+++ src/views/store.vue
@@ -5,15 +5,16 @@
     <a-input @input="handleInput" />
     <!-- <p>{{  inputValue  }}</p> -->
     <a-show :content="inputValue" />
-    <p>appName: {{ appName }}</p>
-    <!-- <p>{{ userName }}</p> -->
+    <!-- <p>appName: {{ appName }}</p> -->
+    <p>userName: {{ userName }}</p>
   </div>
 </template>
 <script>
 import AInput from "_c/AInput.vue";
 import AShow from "_c/AShow.vue";
 
-import { mapState } from "vuex";
+import { createNamespacedHelpers } from "vuex";
+const { mapState } = createNamespacedHelpers("user");
 
 export default {
   name: "store",
@@ -27,8 +28,12 @@ export default {
     AShow
   },
   computed: {
+    // ...mapState({
+    //   appName: state => state.appName,
+    //   userName: state => state.user.userName
+    // })
     ...mapState({
-      appName: state => state.appName
+      userName: state => state.userName
     })
     // appName() {
     //   return this.$store.state.appName;
