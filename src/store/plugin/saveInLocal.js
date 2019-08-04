62f1f3691a59da1b36c4efb88f6b4d42d5e75b40
ccsunnyfd <chzhyfd@126.com>
2019-08-04 13:23:17 +0800

    test persistent storage by plugin localStorage

--- src/store/plugin/saveInLocal.js
+++ src/store/plugin/saveInLocal.js
@@ -1,6 +1,10 @@
 export default store => {
-  console.log('store初始化了')
+  if (localStorage.state) {
+    store.replaceState(JSON.parse(localStorage.state))
+  }
+  // console.log('store初始化了')
   store.subscribe((mutation, state) => {
-    console.log('提交mutation')
+    // console.log('提交mutation')
+    localStorage.state = JSON.stringify(state)
   })
 }
