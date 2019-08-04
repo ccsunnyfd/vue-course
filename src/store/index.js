fde9084f37d5382ef54a19692f99bd1c825c1113
ccsunnyfd <chzhyfd@126.com>
2019-08-04 13:31:02 +0800

    test Vuex mutations strict mode

--- src/store/index.js
+++ src/store/index.js
@@ -10,6 +10,7 @@ import saveInLocal from './plugin/saveInLocal'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict: process.env.NODE_ENV === 'development',  // 开发环境下使用strict: true, 生产环境下希望即便不规范也不要报错，所以用false
   state,
   getters,
   mutations,
