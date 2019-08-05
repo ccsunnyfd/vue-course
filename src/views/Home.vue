4a4ecab6a8b637ae0f705dd904bbf7233d5bf532
ccsunnyfd <chzhyfd@126.com>
2019-08-05 17:56:39 +0800

    test axios proxy post

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,12 +6,14 @@
     <button @click="handleClick('back')">返回上一页</button>
     <button @click="handleClick('push')">跳转到parent</button>
     <button @click="handleClick('replace')">替换到parent</button>
+    <button @click="getInfo">请求数据</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
   name: "home",
@@ -53,6 +55,11 @@ export default {
         this.$router.replace({
           name: "parent"
         });
+    },
+    getInfo() {
+      axios.post("/getUserInfo", { userId: 21 }).then(res => {
+        console.log(res);
+      });
     }
   }
 };
