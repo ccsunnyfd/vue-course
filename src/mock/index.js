0e8e75dd8996f12e1821e3de85452d6535a04e87
ccsunnyfd <chzhyfd@126.com>
2019-08-06 15:01:17 +0800

    test mock using regular expr and template

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,6 +1,6 @@
 import Mock from 'mockjs'
 import { getUserInfo } from './response/user'
 
-Mock.mock('http://localhost:3000/getUserInfo', getUserInfo)
+Mock.mock(/\/getUserInfo/, { name: 'lison' })
 
 export default Mock
