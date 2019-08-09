0d2c728602c3b7445da23b791bd741b7efec3ba3
ccsunnyfd <chzhyfd@126.com>
2019-08-09 16:58:12 +0800

    add render simple example with stopPropagation

--- src/main.js
+++ src/main.js
@@ -8,8 +8,34 @@ if (process.env.NODE_ENV !== 'production') require('./mock')
 Vue.config.productionTip = false
 Vue.prototype.$bus = Bus
 
+const handleClick = event => {
+  console.log(event)
+  event.stopPropagation()
+}
+
+let list = [{ name: 'lison' }, { name: 'lili' }]
+const getLiEleArr = (h) => {
+  return list.map(item => h('li', {
+    on: {
+      'click': handleClick
+    }
+  }, item.name))
+}
+
+
 new Vue({
   router,
   store,
-  render: h => h(App)
+  // render: h => h(App)
+  render: h => h('div', [
+    h('ul', {
+      on: {
+        'click': handleClick
+      }
+    }, getLiEleArr(h))
+  ])
+  // render: h => h('div', [
+  //   h('span', '111'),
+  //   h('span', '222')
+  // ])
 }).$mount('#app')
---
+++ src/views/render-page.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul @click="handleClick">
+      <li @click.stop="handleClick" v-for="(item, index) in list" :key="`list_item_${index}`">{{ item.name }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      list: [{ name: "lison" }, { name: "lili" }]
+    };
+  },
+  methods: {
+    handleClick(event) {
+      console.log(event);
+    }
+  }
+};
+</script>
