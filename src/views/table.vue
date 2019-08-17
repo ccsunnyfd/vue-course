1c1613655856b7a613e6b52db960422e3d125fc7
ccsunnyfd <chzhyfd@126.com>
2019-08-17 08:28:30 +0800

    prepare for editable table component

---
+++ src/views/table.vue
@@ -0,0 +1,29 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { getTableData } from "@/api/data";
+export default {
+  date() {
+    return {
+      tableData: [],
+      columns: [
+        { key: "name", title: "姓名" },
+        { key: "age", title: "年龄", editable: true },
+        { key: "email", title: "邮箱", editable: true }
+      ]
+    };
+  },
+  mounted() {
+    getTableData().then(res => {
+      this.tableData = res;
+    });
+  }
+};
+</script>
+
+<style>
+</style>
+
+
