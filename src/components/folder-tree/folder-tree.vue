ea33c0c6a3fe914ad1fd3b2df598aa33c8e6982c
ccsunnyfd <chzhyfd@126.com>
2019-08-22 15:37:23 +0800

    implement FolderTree advance features

--- src/components/folder-tree/folder-tree.vue
+++ src/components/folder-tree/folder-tree.vue
@@ -3,13 +3,28 @@
 </template>
 
 <script>
-import { putFileInFolder, transferFolderToTree } from "@/lib/util";
+import {
+  putFileInFolder,
+  transferFolderToTree,
+  expandSpecifiedFolder
+} from "@/lib/util";
+import clonedeep from "clonedeep";
 export default {
   name: "FolderTree",
   data() {
     return {
       folderTree: [],
+      currentRenamingId: "",
+      currentRenamingContent: "",
       renderFunc: (h, { root, node, data }) => {
+        const dropList =
+          data.type === "folder" ? this.folderDrop : this.fileDrop;
+        const dropdownRender = dropList.map(item => {
+          return <dropdownItem name={item.name}>{item.title}</dropdownItem>;
+        });
+        const isRenaming =
+          this.currentRenamingId ===
+          `${data.type === "folder" ? "folder" : "file"}_${data.id}`;
         return (
           <div class="tree-item">
             {data.type === "folder" ? (
@@ -21,7 +36,40 @@ export default {
             ) : (
               ""
             )}
-            {data.title}
+            {isRenaming ? (
+              <span>
+                <i-input
+                  value={data.title}
+                  on-input={this.handleInput}
+                  class="tree-rename-input"
+                ></i-input>
+                <i-button
+                  size="small"
+                  type="text"
+                  on-click={this.saveRename.bind(this, data)}
+                >
+                  <icon type="md-checkmark" />
+                </i-button>
+                <i-button size="small" type="text" on-click={this.giveup}>
+                  <icon type="md-close" />
+                </i-button>
+              </span>
+            ) : (
+              <span>{data.title}</span>
+            )}
+            {dropList && !isRenaming ? (
+              <dropdown
+                placement="right-start"
+                on-on-click={this.handleDropdownClick.bind(this, data)}
+              >
+                <i-button size="small" type="text" class="tree-item-button">
+                  <icon type="md-more" size={12} />
+                </i-button>
+                <dropdownMenu slot="list">{dropdownRender}</dropdownMenu>
+              </dropdown>
+            ) : (
+              ""
+            )}
           </div>
         );
       }
@@ -35,7 +83,10 @@ export default {
     fileList: {
       type: Array,
       default: () => []
-    }
+    },
+    folderDrop: Array,
+    fileDrop: Array,
+    beforeDelete: Function
   },
   watch: {
     folderList() {
@@ -50,6 +101,79 @@ export default {
       this.folderTree = transferFolderToTree(
         putFileInFolder(this.folderList, this.fileList)
       );
+    },
+    isFolder(type) {
+      return type === "folder";
+    },
+    handleDelete(data) {
+      const folderId = data.folder_id;
+      const isFolder = this.isFolder(data.type);
+      let updateListName = isFolder ? "folderList" : "fileList";
+      let list = isFolder
+        ? clonedeep(this.folderList)
+        : clonedeep(this.fileList);
+      list = list.filter(item => item.id !== data.id);
+      this.$emit(`update:${updateListName}`, list);
+      this.$nextTick(() => {
+        expandSpecifiedFolder(this.folderTree, folderId);
+      });
+    },
+    handleDropdownClick(data, name) {
+      if (name === "rename") {
+        this.currentRenamingId = `${
+          data.type === "folder" ? "folder" : "file"
+        }_${data.id}`;
+      } else if (name === "delete") {
+        this.$Modal.confirm({
+          title: "提示",
+          content: `您确定要删除${
+            this.isFolder(data.type) ? "文件夹" : "文件"
+          }《${data.title}》吗?`,
+          onOk: () => {
+            this.beforeDelete
+              ? this.beforeDelete()
+                  .then(() => {
+                    this.handleDelete(data);
+                  })
+                  .catch(() => {
+                    this.$Message.error("删除失败");
+                  })
+              : this.handleDelete(data);
+          }
+        });
+      }
+    },
+    handleInput(value) {
+      this.currentRenamingContent = value;
+    },
+    updateList(list, id) {
+      let i = -1;
+      let len = list.length;
+      while (++i < len) {
+        let folderItem = list[i];
+        if (folderItem.id === id) {
+          folderItem.name = this.currentRenamingContent;
+          list.splice(i, 1, folderItem);
+          break;
+        }
+      }
+      return list;
+    },
+    giveup() {
+      this.currentRenamingId = "";
+    },
+    saveRename(data) {
+      const id = data.id;
+      const type = data.type;
+      let i = -1;
+      if (type === "folder") {
+        const list = this.updateList(clonedeep(this.folderList), id);
+        this.$emit("update:folderList", list);
+      } else {
+        const list = this.updateList(clonedeep(this.fileList), id);
+        this.$emit("update:fileList", list);
+      }
+      this.currentRenamingId = "";
     }
   },
   mounted() {
@@ -64,5 +188,18 @@ export default {
   width: ~"calc(100% - 50px)";
   height: 30px;
   line-height: 30px;
+  & > .ivu-dropdown {
+    float: right;
+  }
+  ul.ivu-dropdown-menu {
+    padding-left: 0;
+  }
+  li.ivu-dropdown-item {
+    margin: 0;
+    padding: 7px 16px;
+  }
+  .tree-rename-input {
+    width: ~"calc(100% - 80px)";
+  }
 }
 </style>
--- src/views/folder-tree/folder-tree.vue
+++ src/views/folder-tree/folder-tree.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="folder-wrapper">
     <!-- <Tree :data="folderTree" :render="renderFunc"></Tree> -->
-    <folder-tree :folder-list="folderList" :file-list="fileList" />
+    <folder-tree :folder-list.sync="folderList" :file-list.sync="fileList" :folder-drop="folderDrop" :file-drop="fileDrop" :beforeDelete="beforeDelete" />
   </div>
 </template>
 
@@ -16,7 +16,42 @@ export default {
     return {
       folderList: [],
       fileList: [],
+      folderDrop: [
+        {
+          name: "rename",
+          title: "重命名"
+        },
+        {
+          name: "delete",
+          title: "删除文件夹"
+        }
+      ],
+      fileDrop: [
+        {
+          name: "rename",
+          title: "重命名"
+        },
+        {
+          name: "delete",
+          title: "删除文件"
+        }
+      ]
     };
+  },
+  methods: {
+    beforeDelete() {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          let error = null;
+          // let error = new Error('error')
+          if (!error) {
+            resolve()
+          } else {
+            reject(error)
+          }
+        }, 2000);
+      });
+    }
   },
   mounted() {
     Promise.all([getFolderList(), getFileList()]).then(res => {
